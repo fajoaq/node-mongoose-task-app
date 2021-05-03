f09217c121e62d1a40f06306058cf6076fa4e162
Francis J <fajoaq@gmail.com>
2021-05-03 08:34:09 -0400

    promise chaining test

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,6 +2,7 @@ require('../src/db/mongoose');
 
 const Task = require('../src/models/task');
 
+/* 
 Task.findByIdAndRemove('608d7f78d21041195c7a4ebd').then((task) => {
     console.log(task.description, 'was deleted.');
     return Task.countDocuments({ completed: false });
@@ -9,4 +10,18 @@ Task.findByIdAndRemove('608d7f78d21041195c7a4ebd').then((task) => {
     console.log('A total of', count, ' tasks remain to be completed.');
 }).catch((e) => {
     console.log(e);
-})
+}) 
+*/
+
+const deleteTaskAndCount = async (id) => {
+    await Task.findByIdAndRemove(id);
+    const count = await Task.countDocuments({ completed: false });
+
+    return count;
+};
+
+deleteTaskAndCount('608feb9c011e6612903b70cd').then((count) => {
+    console.log('count:', count);
+}).catch((err) => {
+    console.log(err);
+});
