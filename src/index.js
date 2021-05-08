b99993280c38d36609fc3214e610fe79c213d55d
Francis J <fajoaq@gmail.com>
2021-05-08 11:16:39 -0400

    completed routes, created router files

--- src/index.js
+++ src/index.js
@@ -1,92 +1,16 @@
 const express = require('express');
 require('./db/mongoose');
 
-const User = require('./models/user');
-const Task = require('./models/task');
+const taskRouter = require('./routers/task');
+const userRouter = require('./routers/user');
+const { findByIdAndUpdate } = require('./models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-
-/// USERS
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body);
-
-    try{
-        await user.save();
-
-        res.status(201).send(user);
-    } catch(err) {
-        res.status(400).send(err);
-    }
-});
-
-app.get('/users', async (req, res) => {
-    try {
-        let users = await User.find({});
-
-        res.send(users)
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        let user = await User.findById(_id);
-
-        if(!user) return res.status(404).send();
-
-        res.send(user);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-/// TASKS
-
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body);
-
-    try {
-        await task.save();
-
-        res.status(201).send(task);
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-
-app.get('/tasks', async (req, res) => {
-    try {
-        let tasks = await Task.find({});
-
-        if(!tasks) return res.status(404).send();
-
-        res.send(tasks);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        let task = await Task.findById(_id);
-
-        if(!task) return res.status(404).send();
-
-        res.send(task);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
+app.use(taskRouter);
+app.use(userRouter);
 
 app.listen(port, () => {
     console.log('Server up on port ' +  port);
