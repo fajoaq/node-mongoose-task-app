25554c4e0504cf1ef88e451dcd3efb503dac9ce3
Francis J <fajoaq@gmail.com>
2021-05-12 18:31:15 -0400

    removed get user by id route

--- src/routers/user.js
+++ src/routers/user.js
@@ -61,20 +61,6 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
 });
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        let user = await User.findById(_id);
-
-        if(!user) return res.status(404).send();
-
-        res.send(user);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
 // USER UPDATE
 
 router.patch('/users/:id', async (req, res) => {
