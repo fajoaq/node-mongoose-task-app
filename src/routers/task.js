bb795d4a4bc513888e2582000aadb212d5dd6b2f
Francis J <fajoaq@gmail.com>
2021-05-12 20:12:58 -0400

    limited tasks routes to logged in user

--- src/routers/task.js
+++ src/routers/task.js
@@ -23,13 +23,11 @@ router.post('/tasks', auth, async (req, res) => {
 });
 
 // Get all tasks
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        let tasks = await Task.find({});
+        await req.user.populate('tasks').execPopulate();
 
-        if(!tasks) return res.status(404).send();
-
-        res.send(tasks);
+        res.send(req.user.tasks);
     } catch (err) {
         res.status(500).send(err);
     }
@@ -51,7 +49,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
 });
 
 // TASK UPDATE
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['description', 'completed'];
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
@@ -59,8 +57,7 @@ router.patch('/tasks/:id', async (req, res) => {
     if(!isValidOperation) return res.status(400).send({error: 'Invalid updates.'});
 
     try {
-
-        let task = await Task.findById(req.params.id);
+        let task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
 
         if(!task) return res.status(404).send();
 
@@ -74,9 +71,12 @@ router.patch('/tasks/:id', async (req, res) => {
 });
 
 // TASK DELETE
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id);
+        let task = await Task.findOneAndDelete({ 
+            _id: req.params.id, 
+            owner: req.user._id 
+        });
 
         if(!task) return res.status(404).send();
 
