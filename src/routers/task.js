1c4284bd35bc4cc75092358c73ad6b261a8aa030
Francis J <fajoaq@gmail.com>
2021-05-14 12:18:46 -0400

    added pagination to tasks query

--- src/routers/task.js
+++ src/routers/task.js
@@ -23,6 +23,7 @@ router.post('/tasks', auth, async (req, res) => {
 });
 
 // GET /tasks?completed=true
+//GET /tasks?limit=10&skip=20
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
 
@@ -31,7 +32,11 @@ router.get('/tasks', auth, async (req, res) => {
     try {
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         }).execPopulate();
 
         res.send(req.user.tasks);
