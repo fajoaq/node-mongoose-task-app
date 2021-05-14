a6febb2e83e5c892d942fb53da6174f982de61f9
Francis J <fajoaq@gmail.com>
2021-05-14 13:52:09 -0400

    added sorting by asc and desc to tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -23,11 +23,18 @@ router.post('/tasks', auth, async (req, res) => {
 });
 
 // GET /tasks?completed=true
-//GET /tasks?limit=10&skip=20
+// GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt_asc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
     if(req.query.completed) match.completed = req.query.completed === 'true';
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split('_');
+        //Ascending vs Descending for whichever sort method was provided
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+    }
 
     try {
         await req.user.populate({
@@ -35,7 +42,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate();
 
