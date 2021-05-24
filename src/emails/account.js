b1c12986de6d98bbec98413442e32541b2161972
Francis J <fajoaq@gmail.com>
2021-05-24 13:42:40 -0400

    todo extract domain

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,6 +1,7 @@
 const mailgun = require("mailgun-js");
 
-//mailgun specific
+// mailgun specific
+// TODO: Extract DOMAIN
 const mailGunAPIKey = process.env.MAILGUN_API_KEY;
 const DOMAIN = 'sandboxe86c0a6adfc7434aa24ff56cb3378235.mailgun.org';
 const mg = mailgun({apiKey: mailGunAPIKey, domain: DOMAIN});
