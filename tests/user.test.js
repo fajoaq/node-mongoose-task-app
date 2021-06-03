3f14cd615382f91a9863af1f61124493bd9769fd
Francis J <fajoaq@gmail.com>
2021-06-03 19:00:04 -0400

    configured beforeEach for user testing

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,15 +1,28 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/models/user');
+const mongoose = require('mongoose');
+
+const userOne = {
+    name: 'Francis Tester',
+    email: 'francistester@testingtest.com',
+    password: '56What!!'
+}
+
+beforeEach( async () => {
+    await User.deleteMany();
+    await new User(userOne).save();
+});
+
+
+afterAll(() => {
+    mongoose.connection.close();
+});
 
 test('Should sign up a new user', async () => {
     await request(app).post('/users').send({
         name: 'Francis J',
-        email: 'francis@gmail.com',
+        email: 'francis@yandex.com',
         password: 'MyPass777!'
     }).expect(201);
 });
-
-const mongoose = require('mongoose')
-afterAll(() => {
-    mongoose.connection.close();
-})
